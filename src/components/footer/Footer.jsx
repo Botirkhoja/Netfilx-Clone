c6bc33d8fc6b3d21c63e2c5e2fa519f3a5cd6481
Yunus <obidxojayevbotir@gmail.com>
2023-08-17 18:10:24 +0300

    #fix #fixed #footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,7 +15,6 @@ const Footer = () => {
               <li>FAQ</li>
               <li>Watch List</li>
             </ul>
-            <p>© 2022 STREAMIT. All Rights Reserved. All videos and shows on this platform are trademarks of, and all related images and content are the property of, Streamit Inc. Duplication and copy of this is strictly prohibited. All rights reserved.</p>
           </div>
           <div className='box'>
             <h3>Follow Us</h3>
